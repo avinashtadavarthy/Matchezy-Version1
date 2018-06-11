fb2d3458e19665db42ded6c8bb29453550b2e687
iyashwant <iyashwantkumar@gmail.com>
2018-06-11 14:49:11 +0530

    registration 2 validation

--- app/src/main/java/com/example/yashwant/matchezy/Registration2.java
+++ app/src/main/java/com/example/yashwant/matchezy/Registration2.java
@@ -5,27 +5,40 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 import com.scalified.fab.ActionButton;
 import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 
+import org.json.JSONObject;
+
 import java.util.Calendar;
 
 public class Registration2 extends AppCompatActivity {
 
     EditText editText_gender,editText_interested,editText_city,editText_relationship,editText_lang;
+    private TextInputLayout inputLayoutgender, inputLayoutinterested, inputLayoutrelationship,inputLayoutlang,inputLayoutcity;
 
     private static final int LANGUAGES_SPOKEN = 25;
 
@@ -33,6 +46,12 @@ public class Registration2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration2);
+
+        inputLayoutgender = (TextInputLayout) findViewById(R.id.inputlayout_gender);
+        inputLayoutinterested = (TextInputLayout) findViewById(R.id.inputlayout_interested);
+        inputLayoutrelationship = (TextInputLayout) findViewById(R.id.inputlayout_relationship);
+        inputLayoutlang= (TextInputLayout)findViewById(R.id.inputlayout_lang);
+        inputLayoutcity= (TextInputLayout)findViewById(R.id.inputlayout_city);
 
 
         editText_gender = (EditText)findViewById(R.id.edit_gender);
@@ -44,6 +63,14 @@ public class Registration2 extends AppCompatActivity {
         editText_lang.setShowSoftInputOnFocus(false);
         editText_relationship.setShowSoftInputOnFocus(false);
         editText_interested.setShowSoftInputOnFocus(false);
+
+        editText_gender.addTextChangedListener(new Registration2.MyTextWatcher(editText_gender));
+        editText_city.addTextChangedListener(new Registration2.MyTextWatcher(editText_city));
+        editText_interested.addTextChangedListener(new Registration2.MyTextWatcher(editText_interested));
+        editText_relationship.addTextChangedListener(new Registration2.MyTextWatcher(editText_relationship));
+        editText_lang.addTextChangedListener(new Registration2.MyTextWatcher(editText_lang));
+
+
 
 
         final ActionButton actionButton = (ActionButton) findViewById(R.id.action_button_next2);
@@ -334,4 +361,177 @@ public class Registration2 extends AppCompatActivity {
         }
 
     }
+
+    private void submitForm() {
+        if (!validateGender()) {
+            return;
+        }
+
+
+        if(!validateInterested())
+        {
+            return;
+        }
+
+        if(!validateRelationship()) {
+            return;
+        }
+
+
+        if (!validateCity()) {
+            return;
+        }
+
+        if (!validateLang()) {
+            return;
+        }
+
+/*
+        String date = inputEmail.getText().toString().trim();
+
+
+        AndroidNetworking.post(User.getInstance().BASE_URL+"register")
+                .addBodyParameter("username",inputName.getText().toString().trim())
+                .addBodyParameter("dob",input_Dateofbirth.getText().toString().trim())
+                .addBodyParameter("phone_number",inputPassword.getText().toString().trim())
+                .addBodyParameter("email",input_Dateofbirth.getText().toString().trim())
+                .addBodyParameter("password",input_number.getText().toString().trim())
+                .setPriority(Priority.HIGH)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // do anything with response
+                        Log.e("check",response.toString());
+                        Toast.makeText(getApplicationContext(), "Thank You!", Toast.LENGTH_SHORT).show();
+                    }
+                    @Override
+                    public void onError(ANError error) {
+                        // handle error
+                        error.printStackTrace();
+                    }
+                });*/
+          Intent intent = new Intent(getApplicationContext(),Registration3.class);
+                startActivity(intent);
+
+    }
+
+    private boolean validateGender() {
+        if (editText_gender.getText().toString().trim().isEmpty()) {
+            inputLayoutgender.setError("Enter gender");
+            requestFocus(editText_gender);
+            return false;
+        } else {
+            inputLayoutgender.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+    private boolean validateInterested() {
+        if(editText_interested.getText().toString().trim().isEmpty()) {
+            inputLayoutinterested.setError("Enter interested");
+            requestFocus(editText_interested);
+            return false;
+        }
+
+
+        else {
+            inputLayoutinterested.setErrorEnabled(false);
+        }
+        return true;
+    }
+
+
+    private boolean validateRelationship() {
+        if (editText_relationship.getText().toString().trim().isEmpty()) {
+            inputLayoutrelationship.setError("Enter relationship status");
+            requestFocus(editText_relationship);
+            return false;
+        } else {
+            inputLayoutrelationship.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+    private boolean validateCity() {
+        if (editText_city.getText().toString().trim().isEmpty()) {
+            inputLayoutcity.setError("Enter City");
+            requestFocus(editText_city);
+            return false;
+        } else {
+            inputLayoutrelationship.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+    private boolean validateLang() {
+        if (editText_lang.getText().toString().trim().isEmpty()) {
+            inputLayoutgender.setError(getString(R.string.err_msg_password));
+            requestFocus(editText_lang);
+            return false;
+        } else {
+            inputLayoutlang.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+
+    private void requestFocus(View view) {
+        if (view.requestFocus()) {
+            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
+    }
+
+    private class MyTextWatcher implements TextWatcher {
+
+        private View view;
+
+        private MyTextWatcher(View view) {
+            this.view = view;
+        }
+
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void afterTextChanged(Editable editable) {
+            switch (view.getId()) {
+                case R.id.edit_gender:
+                    validateGender();
+                    break;
+
+
+                case R.id.edit_interested:
+                    validateInterested();
+                    break;
+
+                case R.id.edit_relationship:
+                    validateRelationship();
+                    break;
+
+
+                case R.id.edit_city:
+                    validateCity();
+                    break;
+
+
+                case R.id.edit_lang:
+                    validateLang();
+                    break;
+
+
+
+
+
+            }
+        }
+    }
+
+
 }
