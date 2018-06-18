aead0a91df71761a67cf57bec557fc106da8fc8a
iyashwant <iyashwantkumar@gmail.com>
2018-06-18 14:44:22 +0530

    chip

--- app/src/main/java/com/example/yashwant/matchezy/ProfilePage.java
+++ app/src/main/java/com/example/yashwant/matchezy/ProfilePage.java
@@ -2,12 +2,17 @@ package com.example.yashwant.matchezy;
 
 import android.graphics.Color;
 import android.os.Build;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.scalified.fab.ActionButton;
 
-public class ProfilePage extends AppCompatActivity {
+public class ProfilePage extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,5 +42,31 @@ public class ProfilePage extends AppCompatActivity {
         actionButton2.playShowAnimation();
         actionButton2.setImageResource(R.drawable.ic_action_dislike);
 
+        ViewPager pager = (ViewPager) findViewById(R.id.viewPager);
+        pager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+
     }
-}
+
+    private class MyPagerAdapter extends FragmentPagerAdapter {
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int pos) {
+            switch(pos) {
+
+                case 0: return Fragment_profileInterests.newInstance("FirstFragment, Instance 1");
+                case 1: return Fragment_profileInfo.newInstance("SecondFragment, Instance 1");
+                case 2: return Fragment_profileBio.newInstance("ThirdFragment, Instance 1");
+                default: return Fragment_profileInterests.newInstance("ThirdFragment, Default");
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+    }
+    }
