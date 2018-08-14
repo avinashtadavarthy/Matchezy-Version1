415449a2c84344518cec544704aee318f92221ff
Hariprasanth <shhariprasanth@gmail.com>
2018-08-14 22:38:57 +0530

    Modify orientation of rotated images when selected from gallery

--- app/src/main/java/com/einheit/matchezy/registration/Registration_Imageupload.java
+++ app/src/main/java/com/einheit/matchezy/registration/Registration_Imageupload.java
@@ -9,9 +9,7 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
@@ -49,7 +47,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -317,10 +314,7 @@ public class Registration_Imageupload extends AppCompatActivity {
                     Bitmap bm = null;
 
                     try {
-
-                        bm = getCorrectlyOrientedImage(Registration_Imageupload.this, selectedImageUri, 500);
-                        //bm = MediaStore.Images.Media.getBitmap(Registration_Imageupload.this.getContentResolver(), selectedImageUri);
-
+                        bm = MediaStore.Images.Media.getBitmap(Registration_Imageupload.this.getContentResolver(), selectedImageUri);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
@@ -334,6 +328,12 @@ public class Registration_Imageupload extends AppCompatActivity {
                     boolean success = true;
                     if (!storageDir.exists()) {
                         success = storageDir.mkdirs();
+                    }
+
+                    try {
+                        bm = Utility.modifyOrientation(bm, Utility.getPath(this, selectedImageUri));
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
 
                     try {
@@ -414,219 +414,6 @@ public class Registration_Imageupload extends AppCompatActivity {
 
         return data;
 
-    }
-
-    public static String getPath(final Context context, final Uri uri) {
-
-        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
-
-        // DocumentProvider
-        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
-            // ExternalStorageProvider
-            if (isExternalStorageDocument(uri)) {
-                final String docId = DocumentsContract.getDocumentId(uri);
-                final String[] split = docId.split(":");
-                final String type = split[0];
-
-                if ("primary".equalsIgnoreCase(type)) {
-                    return Environment.getExternalStorageDirectory() + "/" + split[1];
-                }
-
-                // TODO handle non-primary volumes
-            }
-            // DownloadsProvider
-            else if (isDownloadsDocument(uri)) {
-
-                final String id = DocumentsContract.getDocumentId(uri);
-                final Uri contentUri = ContentUris.withAppendedId(
-                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
-
-                return getDataColumn(context, contentUri, null, null);
-            }
-            // MediaProvider
-            else if (isMediaDocument(uri)) {
-                final String docId = DocumentsContract.getDocumentId(uri);
-                final String[] split = docId.split(":");
-                final String type = split[0];
-
-                Uri contentUri = null;
-                if ("image".equals(type)) {
-                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-                } else if ("video".equals(type)) {
-                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
-                } else if ("audio".equals(type)) {
-                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-                }
-
-                final String selection = "_id=?";
-                final String[] selectionArgs = new String[]{
-                        split[1]
-                };
-
-                return getDataColumn(context, contentUri, selection, selectionArgs);
-            }
-        }
-        // MediaStore (and general)
-        else if ("content".equalsIgnoreCase(uri.getScheme())) {
-
-            // Return the remote address
-            if (isGooglePhotosUri(uri))
-                return uri.getLastPathSegment();
-
-            return getDataColumn(context, uri, null, null);
-        }
-        // File
-        else if ("file".equalsIgnoreCase(uri.getScheme())) {
-            return uri.getPath();
-        }
-
-        return null;
-    }
-
-    /**
-     * Get the value of the data column for this Uri. This is useful for
-     * MediaStore Uris, and other file-based ContentProviders.
-     *
-     * @param context       The context.
-     * @param uri           The Uri to query.
-     * @param selection     (Optional) Filter used in the query.
-     * @param selectionArgs (Optional) Selection arguments used in the query.
-     * @return The value of the _data column, which is typically a file path.
-     */
-    public static String getDataColumn(Context context, Uri uri, String selection,
-                                       String[] selectionArgs) {
-
-        Cursor cursor = null;
-        final String column = "_data";
-        final String[] projection = {
-                column
-        };
-
-        try {
-            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
-                    null);
-            if (cursor != null && cursor.moveToFirst()) {
-                final int index = cursor.getColumnIndexOrThrow(column);
-                return cursor.getString(index);
-            }
-        } finally {
-            if (cursor != null)
-                cursor.close();
-        }
-        return null;
-    }
-
-
-
-    //code to check for image orientation and adjust it
-    public static int getOrientation(Context context, Uri photoUri) {
-
-        Cursor cursor = context.getContentResolver().query(photoUri,
-                new String[] { MediaStore.Images.ImageColumns.ORIENTATION }, null, null, null);
-
-        if (cursor == null || cursor.getCount() != 1) {
-            return 90;  //Assuming it was taken portrait
-        }
-
-        cursor.moveToFirst();
-        return cursor.getInt(0);
-    }
-
-    /**
-     * Rotates and shrinks as needed
-     */
-    public static Bitmap getCorrectlyOrientedImage(Context context, Uri photoUri, int maxWidth)
-            throws IOException {
-
-        InputStream is = context.getContentResolver().openInputStream(photoUri);
-        BitmapFactory.Options dbo = new BitmapFactory.Options();
-        dbo.inJustDecodeBounds = true;
-        BitmapFactory.decodeStream(is, null, dbo);
-        is.close();
-
-
-        int rotatedWidth, rotatedHeight;
-        int orientation = getOrientation(context, photoUri);
-
-        if (orientation == 90 || orientation == 270) {
-            Log.d("ImageUtil", "Will be rotated");
-            rotatedWidth = dbo.outHeight;
-            rotatedHeight = dbo.outWidth;
-        } else {
-            rotatedWidth = dbo.outWidth;
-            rotatedHeight = dbo.outHeight;
-        }
-
-        Bitmap srcBitmap;
-        is = context.getContentResolver().openInputStream(photoUri);
-        Log.d("ImageUtil", String.format("rotatedWidth=%s, rotatedHeight=%s, maxWidth=%s",
-                rotatedWidth, rotatedHeight, maxWidth));
-        if (rotatedWidth > maxWidth || rotatedHeight > maxWidth) {
-            float widthRatio = ((float) rotatedWidth) / ((float) maxWidth);
-            float heightRatio = ((float) rotatedHeight) / ((float) maxWidth);
-            float maxRatio = Math.max(widthRatio, heightRatio);
-            Log.d("ImageUtil", String.format("Shrinking. maxRatio=%s", maxRatio));
-
-            // Create the bitmap from file
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            options.inSampleSize = (int) maxRatio;
-            srcBitmap = BitmapFactory.decodeStream(is, null, options);
-        } else {
-            Log.d("ImageUtil", String.format("No need for Shrinking. maxRatio=%s", 1));
-
-            srcBitmap = BitmapFactory.decodeStream(is);
-            Log.d("ImageUtil", String.format("Decoded bitmap successful"));
-        }
-        is.close();
-
-        /*
-         * if the orientation is not 0 (or -1, which means we don't know), we
-         * have to do a rotation.
-         */
-        if (orientation > 0) {
-            Matrix matrix = new Matrix();
-            matrix.postRotate(orientation);
-
-            srcBitmap = Bitmap.createBitmap(srcBitmap, 0, 0, srcBitmap.getWidth(), srcBitmap.getHeight(), matrix, true);
-        }
-
-        return srcBitmap;
-    }
-    //code to check for image orientation and adjust it - end
-
-
-
-
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is ExternalStorageProvider.
-     */
-    public static boolean isExternalStorageDocument(Uri uri) {
-        return "com.android.externalstorage.documents".equals(uri.getAuthority());
-    }
-
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is DownloadsProvider.
-     */
-    public static boolean isDownloadsDocument(Uri uri) {
-        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
-    }
-
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is MediaProvider.
-     */
-    public static boolean isMediaDocument(Uri uri) {
-        return "com.android.providers.media.documents".equals(uri.getAuthority());
-    }
-
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is Google Photos.
-     */
-    public static boolean isGooglePhotosUri(Uri uri) {
-        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
     }
 
     public boolean checkPermissionForReadExtertalStorage() {
